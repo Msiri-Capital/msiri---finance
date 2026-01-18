93f0636f636c4803629fdb96a9b9bbff0f27b253
Msiri-Capital <ncl5555lvt@gmail.com>
2026-01-18 13:54:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,8 @@ data_perf = {
 st.table(data_perf)  
 
     # 3. SECTION COMMENTAIRES (C'est ici que ça bloquait)
-    
+
+    st.divided()
     st.header("👥 Communauté M'SIRI : Déjà +120 Membres VIP")
     
     col_stat1, col_stat2, col_stat3 = st.columns(3)
